1e11e47e08ce3d0fa7de97f7c7e74ff78c0347e6
Joau0824 <a20182162@pucp.edu.pe>
2022-12-01 00:24:04 -0500

    fragment

---
+++ app/src/main/java/com/example/lendti/Adapter/ListadeSolicitudesAprobadasAdapter.java
@@ -0,0 +1,25 @@
+package com.example.lendti.Adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lendti.Entity.Equipo;
+import com.example.lendti.Entity.Solicitudes;
+import com.example.lendti.R;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+public class ListadeSolicitudesAprobadasAdapter{
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/lendti/Entity/Solicitudes.java
@@ -0,0 +1,8 @@
+package com.example.lendti.Entity;
+
+public class Solicitudes {
+    private String tipo;
+    private String marca;
+    private String tiempoPrestamo;
+    private String estado;
+}
