0c62a7c39fb1c7209628f775031bb6d403282224
Darwin Aponte <darwin.aponte@pucp.edu.pe>
2022-12-01 23:40:51 -0500

    entity userit

---
+++ app/src/main/java/com/example/lendti/Entity/UserIT.java
@@ -0,0 +1,50 @@
+package com.example.lendti.Entity;
+
+public class UserIT {
+
+    public UserIT(String nombre, String correo, String codigo, String password) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.codigo = codigo;
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String nombre;
+    private String correo;
+    private String codigo;
+    private String password;
+
+
+}
